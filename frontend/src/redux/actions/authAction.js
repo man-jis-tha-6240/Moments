48496bca3a94d89f9b844db4bb6cf23cfac01890
Manjistha Dhar <76396648+dharmanjistha12@users.noreply.github.com>
2023-04-16 00:23:00 +0530

    Update authAction.js

--- frontend/src/redux/actions/authAction.js
+++ frontend/src/redux/actions/authAction.js
@@ -27,33 +27,6 @@ export const login = (data) => async (dispatch) => {
 	}
 }
 
-export const refreshToken = () => async (dispatch) => {
-	const firstLogin = localStorage.getItem("firstLogin")
-	if(firstLogin){
-        dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true} })
-        try {
-            const res = await postDataAPI('refresh_token');
-            dispatch({ 
-                type: GLOBALTYPES.AUTH, 
-                payload: {
-                    token: res.data.access_token,
-                    user: res.data.user
-                } 
-            })
-
-            dispatch({ type: GLOBALTYPES.ALERT, payload: {} })
-
-        } catch (err) {
-            dispatch({ 
-                type: GLOBALTYPES.ALERT, 
-                payload: {
-                    error: err.response.data.msg
-                } 
-            })
-        }
-    }
-}
-
 export const signup = (data) => async (dispatch) => {
 	try {
 		const check = valid(data)
